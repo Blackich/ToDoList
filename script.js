4a9546e8113dd26f34dbc98974f32b5f9f5ba9e1
OnlyBlack <aera.onlyblack@gmail.com>
2022-08-31 17:55:53 +0500

    error correct

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-const output = document.querySelector('.output'),
-    btnAdd = document.querySelector('.btn-add');
+const output = document.querySelector('.output');
+const btnAdd = document.querySelector('.btn-add');
 
-let tasks;
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
-
+const tasks = !localStorage.tasks ? [] : JSON.parse(localStorage.getItem('tasks'));
 
 function TodoTask(text) {
-
-    this.text = text;
-    this.done = false;
+  this.text = text;
+  this.done = false;
 }
-
 
 function setId(tasks) {
-
-    return tasks.length > 0
-        ? tasks.length
-        : 0;
+  return tasks.length > 0
+    ? tasks.length
+    : 0;
 }
 
-
-function outputTemplate() {
-
-    return `
+function outputTemplate(tasks) {
+  return `
     <div class="task-list" id="${setId(tasks)}">
         <div class="task" contenteditable="true"></div>
         <div class="btn-menu" type="button" hidden>menu
@@ -36,24 +29,8 @@ function outputTemplate() {
     </div>`;
 }
 
-
-function otrisovkaEbat(arrValues) {
-
-    if (tasks.length > 0) {
-
-        tasks.forEach((btnSave, index) => {
-            output.insertAdjacentHTML('afterbegin', outputTemplateTaTaTavoRot(btnSave, index));
-        });
-    }
-}
-
-
-otrisovkaEbat(tasks);
-
-
-function outputTemplateTaTaTavoRot(btnSave, index) {
-
-    return `
+function outputTemplateRender(btnSave, index) {
+  return `
     <div class="task-list" id="${index}">
         <div class="task" contenteditable="false" style="border: none">${btnSave.text}</div>
         <div class="btn-menu" type="button">menu
@@ -67,149 +44,116 @@ function outputTemplateTaTaTavoRot(btnSave, index) {
     </div>`;
 }
 
-
-
-
-function updateLocal() {
-
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+function localRender(tasks) {
+  if (tasks.length > 0) {
+    tasks.forEach((btnSave, index) => {
+      output.insertAdjacentHTML('afterbegin', outputTemplateRender(btnSave, index));
+    });
+  }
 }
 
+localRender(tasks);
 
-btnAdd.addEventListener('click', (e) => {
+function updateLocal() {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
 
-    output.insertAdjacentHTML('afterbegin', outputTemplate());
-    document.querySelectorAll('.task')[0].focus();
-    btnAdd.disabled = true;
+btnAdd.addEventListener('click', () => {
+  output.insertAdjacentHTML('afterbegin', outputTemplate());
+  document.querySelectorAll('.task')[0].focus();
+  btnAdd.disabled = true;
 });
-
-
-
 
 const btnsSave = document.getElementsByClassName('btn-save');
 document.addEventListener('click', (e) => {
-    
-    for (let btnSave of btnsSave) {
+  for (const btnSave of btnsSave) {
+    if (e.target !== btnSave) continue;
 
-        if (e.target !== btnSave) continue;
+    if (!tasks[btnSave.parentElement.id]) {
+      if (btnSave.parentElement.firstElementChild.textContent) {
+        tasks.push(new TodoTask(btnSave.parentElement.firstElementChild.textContent));
+        updateLocal();
 
-        if (!tasks[btnSave.parentElement.id]) {
+        btnSave.parentElement.firstElementChild.textContent = tasks[btnSave.parentElement.id].text;
+        btnSave.parentElement.firstElementChild.contentEditable = false; // task
+        btnSave.parentElement.firstElementChild.style.border = 'hidden'; // task
+        btnSave.nextElementSibling.hidden = true; // btnCancel
+        btnSave.previousElementSibling.hidden = false; // btnMenu
+        btnSave.hidden = true;
+        btnAdd.disabled = false;
+      } else {
+        btnSave.parentElement.firstElementChild.focus();
+      }
+    } else if (btnSave.parentElement.firstElementChild.textContent) {
+      tasks[btnSave.parentElement.id].text = btnSave.parentElement.firstElementChild.textContent;
+      updateLocal();
 
-            if (btnSave.parentElement.firstElementChild.textContent) {
-                tasks.push(new TodoTask(btnSave.parentElement.firstElementChild.textContent));
-                updateLocal();
-
-                btnSave.parentElement.firstElementChild.textContent = tasks[btnSave.parentElement.id].text; //text = tasks[i]
-                btnSave.parentElement.firstElementChild.contentEditable = false;   //task
-                btnSave.parentElement.firstElementChild.style.border = 'hidden';   //task
-                btnSave.nextElementSibling.hidden = true;   //btnCancel
-                btnSave.previousElementSibling.hidden = false;   //btnMenu
-                btnSave.hidden = true;
-                btnAdd.disabled = false;
-
-            } else {
-
-                btnSave.parentElement.firstElementChild.focus();
-            }
-
-        } else {
-
-            if (btnSave.parentElement.firstElementChild.textContent) {
-
-                tasks[btnSave.parentElement.id].text = btnSave.parentElement.firstElementChild.textContent;   //tasks[i] = text
-                updateLocal();
-
-                btnSave.parentElement.firstElementChild.contentEditable = false;   //task
-                btnSave.parentElement.firstElementChild.style.border = 'hidden';   //task
-                btnSave.nextElementSibling.hidden = true;   //btnCancel
-                btnSave.previousElementSibling.hidden = false;   //btnMenu
-                btnSave.hidden = true;
-                btnAdd.disabled = false;
-
-            } else {
-
-                btnSave.parentElement.firstElementChild.focus();
-            }
-        }
-    } 
+      btnSave.parentElement.firstElementChild.contentEditable = false; // task
+      btnSave.parentElement.firstElementChild.style.border = 'hidden'; // task
+      btnSave.nextElementSibling.hidden = true; // btnCancel
+      btnSave.previousElementSibling.hidden = false; // btnMenu
+      btnSave.hidden = true;
+      btnAdd.disabled = false;
+    } else {
+      btnSave.parentElement.firstElementChild.focus();
+    }
+  }
 }); // <-- btnsSave
-
-
-
 
 const btnsCancel = document.getElementsByClassName('btn-cancel');
 document.addEventListener('click', (e) => {
-    
-    for (let btnCancel of btnsCancel) {
+  for (const btnCancel of btnsCancel) {
+    if (e.target !== btnCancel) continue;
 
-        if (e.target !== btnCancel) continue;
-
-
-        if (!tasks[btnCancel.parentElement.id]) {
-
-            output.removeChild(btnCancel.parentElement);
-            btnAdd.disabled = false;
-
-        } else {
-
-            btnCancel.parentElement.firstElementChild.textContent = tasks[btnCancel.parentElement.id].text;
-            btnCancel.parentElement.firstElementChild.contentEditable = false;   //task
-            btnCancel.parentElement.firstElementChild.style.border = 'hidden';   //task
-            btnCancel.previousElementSibling.hidden = true;   //btnSave
-            btnCancel.parentElement.children[1].hidden = false;   //btnMenu
-            btnCancel.hidden = true;
-            btnAdd.disabled = false;
-        }
-    } 
+    if (!tasks[btnCancel.parentElement.id]) {
+      output.removeChild(btnCancel.parentElement);
+      btnAdd.disabled = false;
+    } else {
+      btnCancel.parentElement.firstElementChild.textContent = tasks[btnCancel.parentElement.id].text;
+      btnCancel.parentElement.firstElementChild.contentEditable = false; // task
+      btnCancel.parentElement.firstElementChild.style.border = 'hidden'; // task
+      btnCancel.previousElementSibling.hidden = true; // btnSave
+      btnCancel.parentElement.children[1].hidden = false; // btnMenu
+      btnCancel.hidden = true;
+      btnAdd.disabled = false;
+    }
+  }
 }); // <-- btnsCancel
-
-
-
 
 const tasksList = document.getElementsByClassName('task-list');
 const btnsDelete = document.getElementsByClassName('btn-delete');
 document.addEventListener('click', (e) => {
+  for (const btnDelete of btnsDelete) {
+    if (e.target !== btnDelete) continue;
 
-    for (let btnDelete of btnsDelete) {
-        
-        if (e.target !== btnDelete) continue;
+    for (const taskList of tasksList) {
+      if (taskList.id > btnDelete.closest('.task-list').id) taskList.id -= 1;
+    }
 
-        for (let taskList of tasksList) {
-
-            if (taskList.id > btnDelete.closest('.task-list').id) taskList.id -= 1;
-        }
-
-        tasks.splice(btnDelete.closest('.task-list').id, 1);
-        updateLocal();
-        output.removeChild(btnDelete.closest('.task-list'));
-    } 
+    tasks.splice(btnDelete.closest('.task-list').id, 1);
+    updateLocal();
+    output.removeChild(btnDelete.closest('.task-list'));
+  }
 }); // <-- btnsDelete
-
-
-
 
 const btnsChange = document.getElementsByClassName('btn-change');
 document.addEventListener('click', (e) => {
-    
-    for (let btnChange of btnsChange) {
+  for (const btnChange of btnsChange) {
+    if (e.target !== btnChange) continue;
 
-        if (e.target !== btnChange) continue;
+    const parentListBtnChange = btnChange.closest('.task-list');
+    btnChange.closest('.btn-menu').hidden = true;
 
-        const parentListBtnChange = btnChange.closest('.task-list');
-        btnChange.closest('.btn-menu').hidden = true;
-
-        parentListBtnChange.firstElementChild.contentEditable = true;   //task content
-        parentListBtnChange.firstElementChild.style.border = '1px solid';   //task border
-        parentListBtnChange.children[2].hidden = false;   //btnSave
-        parentListBtnChange.lastElementChild.hidden = false;   //btnCancel
-        parentListBtnChange.firstElementChild.focus();   //task focus
-    } 
+    parentListBtnChange.firstElementChild.contentEditable = true; // task content
+    parentListBtnChange.firstElementChild.style.border = '1px solid'; // task border
+    parentListBtnChange.children[2].hidden = false; // btnSave
+    parentListBtnChange.lastElementChild.hidden = false; // btnCancel
+    parentListBtnChange.firstElementChild.focus(); // task focus
+  }
 }); // <-- btnsChange
 
-
-
-//clear Storage
+// clear Storage
 const clear = document.querySelector('.clear');
-clear.addEventListener('click', (e) => {
-    localStorage.clear();
+clear.addEventListener('click', () => {
+  localStorage.clear();
 });
